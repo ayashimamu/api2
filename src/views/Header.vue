055f4fe9d9a6c83be51cd643746289c09118f804
島村彩 <ayashimamura@shimamuraayanoMacBook-Air.local>
2020-10-29 13:19:52 +0900

    first-commit

---
+++ src/views/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="header">
+    <button @click="showAddress" type="submit">住所自動入力</button>
+    <p>Address:{{show}}</p>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default{
+
+  data(){
+    return{
+      show:""
+  };
+},
+props:["parentText"],
+methods:{
+  showAddress(){
+    this.show = this.parentText;
+  }
+},
+async created(){
+  let item =await axios.get(
+  `https://apis.postcode-jp.com/api/v4/postcodes/${this.parentText}?apikey=Nz1PrzkqP5lyOdPF2kbhSIuNgjdZXCgAoXBPmqf`
+  );
+this.data = item;
+}
+};
+</script>
